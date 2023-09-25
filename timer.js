223870aca7921e4cfaac47de3974f6502e85ea06
welldowel <welldowell42@gmail.com>
2023-09-25 20:10:45 -0300

    Finalizando o cronômetro

--- timer.js
+++ timer.js
@@ -1,75 +1,27 @@
 let level = 'expert'; // variável que será modificada na seleção de dificuldade
+let tempoRestante = 0; 
 
 if (level == 'expert') {
-
-    let tempoRestante = 600; // 10 minutos
-
-    let cronometro = setInterval(function() {
-        let minutos = Math.floor(tempoRestante / 60);
-        let segundos = tempoRestante % 60;
-
-        document.getElementById('cronus').textContent = 
-            (minutos < 10 ? '0' : '') + minutos + ':' + (segundos < 10 ? '0' : '') + segundos;
-
-        if (tempoRestante <= 0) {
-            clearInterval(cronometro);
-        } else {
-            tempoRestante--;
-        }
-    }, 1000);
-
+    tempoRestante = 600; // 10 minutos
 } else if (level == 'hard') {
-
-    let tempoRestante = 300; // 5 minutos
-
-    let cronometro = setInterval(function() {
-        let minutos = Math.floor(tempoRestante / 60);
-        let segundos = tempoRestante % 60;
-
-        document.getElementById('cronus').textContent = 
-            (minutos < 10 ? '0' : '') + minutos + ':' + (segundos < 10 ? '0' : '') + segundos;
-
-        if (tempoRestante <= 0) {
-            clearInterval(cronometro);
-        } else {
-            tempoRestante--;
-        }
-    }, 1000);
-
+    tempoRestante = 300; // 5 minutos
 } else if (level == 'normal') {
-
-    let tempoRestante = 120; // 2 minutos
-
-    let cronometro = setInterval(function() {
-        let minutos = Math.floor(tempoRestante / 60);
-        let segundos = tempoRestante % 60;
-
-        document.getElementById('cronus').textContent = 
-            (minutos < 10 ? '0' : '') + minutos + ':' + (segundos < 10 ? '0' : '') + segundos;
-
-        if (tempoRestante <= 0) {
-            clearInterval(cronometro);
-        } else {
-            tempoRestante--;
-        }
-    }, 1000);
-
+    tempoRestante = 120; // 2 minutos
 } else if (level == 'easy') {
-
-    let tempoRestante = 60; // 1 minutos
-
-    let cronometro = setInterval(function() {
-        let minutos = Math.floor(tempoRestante / 60);
-        let segundos = tempoRestante % 60;
-
-        document.getElementById('cronus').textContent = 
-            (minutos < 10 ? '0' : '') + minutos + ':' + (segundos < 10 ? '0' : '') + segundos;
-
-        if (tempoRestante <= 0) {
-            clearInterval(cronometro);
-        } else {
-            tempoRestante--;
-        }
-    }, 1000);
-
+    tempoRestante = 60;
 }
+
+let cronometro = setInterval(function() {
+    let minutos = Math.floor(tempoRestante / 60);
+    let segundos = tempoRestante % 60;
+
+    document.getElementById('cronus').textContent = 
+        (minutos < 10 ? '0' : '') + minutos + ':' + (segundos < 10 ? '0' : '') + segundos;
+
+    if (tempoRestante <= 0) {
+        clearInterval(cronometro);
+    } else {
+        tempoRestante--;
+    }
+}, 1000);
+
