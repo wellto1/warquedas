9224259cd9d6d5854a0ed4b762cb0961727823c5
welldowel <welldowell42@gmail.com>
2023-09-25 19:53:04 -0300

    Corrigindo o projeto

--- timer.js
+++ timer.js
@@ -1,26 +1,15 @@
-function iniciarCronometro() {
-    // Define o tempo limite para 2 minutos
-    var tempoLimite = 2 * 60;
+let tempoRestante = 300; // 15 minutos
 
-    // Inicia o cronômetro
-    var segundosRestantes = tempoLimite;
-    var intervalo = setInterval(function() {
-        // Decrementa o tempo restante
-        segundosRestantes--;
+let cronometro = setInterval(function() {
+    let minutos = Math.floor(tempoRestante / 60);
+    let segundos = tempoRestante % 60;
 
-        // Atualiza a exibição do cronômetro
-        document.getElementById("cronometro").innerHTML = segundosRestantes;
+    document.getElementById('cronus').textContent = 
+        (minutos < 10 ? '0' : '') + minutos + ':' + (segundos < 10 ? '0' : '') + segundos;
 
-        // Verifica se o tempo acabou
-        if (segundosRestantes <= 0) {
-            // O tempo acabou
-            clearInterval(intervalo);
-            alert("O tempo acabou!");
-        }
-
-        let cronometro = document.getElementById('clo').innerHTML = `<strong> ${segundosRestantes} </strong>`;
-    }, 1000);
-}
-
-iniciarCronometro();
-
+    if (tempoRestante <= 0) {
+        clearInterval(cronometro);
+    } else {
+        tempoRestante--;
+    }
+}, 1000);
